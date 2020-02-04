8d8aad503defbd01c6cb0d522f7e0626cb6ebebf
Greg Maxey <gregorymaxey@yahoo.com>
2020-02-04 17:09:07 +0000

    Retrieve git

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,5 +1,8 @@
 import groovy.json.*
 node{
+	step(checkout([$class: 'GitSCM', branches: [[name: '*/master']],
+    userRemoteConfigs: [[url: 'https://github.com/cb-demos/JenkinsTriggerFlowSample.git']]]))
+
     def flowdsl = new File("FlowPipeline.groovy").text
 	def body = new JsonBuilder( [overwrite: true, dsl: flowdsl] ).toString()
 	step([$class: 'ElectricFlowGenericRestApi', 
