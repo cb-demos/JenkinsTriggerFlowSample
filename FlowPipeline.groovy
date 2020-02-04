a74d0e3e4c19cfe285c049b9f646663a29d40781
Greg Maxey <gregorymaxey@yahoo.com>
2020-02-04 23:03:55 +0000

    Parameter misnameed

--- FlowPipeline.groovy
+++ FlowPipeline.groovy
@@ -1,32 +1,30 @@
 project 'Kubernetes Example',{
 	pipeline "Jenkins-triggered",{
-	formalParameter "InputParam"
-	stage "Dev",{
-	
-		task "Approve",{
-			taskType = 'MANUAL'
-			approver = [
-				'admin',
-			]
-		}		
-	
-		task 'Deploy App', {
-			description = ''
-			actionLabelText = null
-			actualParameter = [
-				'ec_enforceDependencies': '1',
-				'ec_smartDeployOption': '1',
-				'ec_stageArtifacts': '0',
-				'nginx_version': '1.7.9',
-			]
-			environmentName = 'dev'
-			environmentProjectName = projectName
-			subapplication = 'Kubernetes App'
-			subprocess = 'Deploy'
-			subproject = projectName
-			taskProcessType = 'APPLICATION'
-			taskType = 'PROCESS'
+		formalParameter "nginx_version"
+		stage "Dev",{
+		
+			task "Approve",{
+				taskType = 'MANUAL'
+				approver = [
+					'admin',
+				]
+			}		
+		
+			task 'Deploy App', {
+				actualParameter = [
+					'ec_enforceDependencies': '1',
+					'ec_smartDeployOption': '1',
+					'ec_stageArtifacts': '0',
+					'nginx_version': '1.7.9',
+				]
+				environmentName = 'dev'
+				environmentProjectName = projectName
+				subapplication = 'Kubernetes App'
+				subprocess = 'Deploy'
+				subproject = projectName
+				taskProcessType = 'APPLICATION'
+				taskType = 'PROCESS'
+			}
 		}
-	}
 	}
 }
