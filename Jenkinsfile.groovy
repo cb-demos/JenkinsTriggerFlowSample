de920f3d5de0171dfb37549f7f7436ad11b9a1be
Greg Maxey <gregorymaxey@yahoo.com>
2020-02-04 17:38:03 +0000

    Revert checkout

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,7 +1,7 @@
 import groovy.json.*
 
 node{
-	step([$class: 'GitSCM', branches: [[name: '*/master']],userRemoteConfigs: [[url: 'https://github.com/cb-demos/JenkinsTriggerFlowSample.git']]])
+	step(checkout([$class: 'GitSCM', branches: [[name: '*/master']],userRemoteConfigs: [[url: 'https://github.com/cb-demos/JenkinsTriggerFlowSample.git']]]))
 	
 	def flowdsl = new File("FlowPipeline.groovy").text
 	def body = new JsonBuilder( [overwrite: true, dsl: flowdsl] ).toString()	
