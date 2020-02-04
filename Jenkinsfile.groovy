b8d4eb21586fcaec7554cf0c494cbb76de0bf068
Greg Maxey <gregorymaxey@yahoo.com>
2020-02-04 17:18:17 -0500

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,7 +2,7 @@ import groovy.json.*
 
 node{
 	step(checkout([$class: 'GitSCM', branches: [[name: '*/master']],userRemoteConfigs: [[url: 'https://github.com/cb-demos/JenkinsTriggerFlowSample.git']]]))
-	
+	/*
 	def flowdsl = new File("FlowPipeline.groovy").text
 	def body = new JsonBuilder( [overwrite: true, dsl: flowdsl] ).toString()	
 	step([$class: 'ElectricFlowGenericRestApi', 
@@ -12,7 +12,7 @@ node{
 			body : body,
 			envVarNameForResult:'FLOW_REST_RESPONSE'
 		])
-		
+	*/	
 	step([$class: 'ElectricFlowPipelinePublisher', 
 		configuration: 'Colocated Flow',
 		projectName: 'Default',
