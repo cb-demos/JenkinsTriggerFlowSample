5d9577192cca7eeabe373d47575854373a047e01
Greg Maxey <gregorymaxey@yahoo.com>
2020-02-04 17:28:59 +0000

    Move defs out of closure

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,17 +3,16 @@ import groovy.json.*
 node{
 	step(checkout([$class: 'GitSCM', branches: [[name: '*/master']],userRemoteConfigs: [[url: 'https://github.com/cb-demos/JenkinsTriggerFlowSample.git']]]))
 	
-
+	def flowdsl = new File("FlowPipeline.groovy").text
+	def body = new JsonBuilder( [overwrite: true, dsl: flowdsl] ).toString()	
 	step([$class: 'ElectricFlowGenericRestApi', 
 			configuration: 'Colocated Flow',
 			urlPath : '/server/dsl',
 			httpMethod : 'POST',
 			body : body,
 			envVarNameForResult:'FLOW_REST_RESPONSE'
-		]) {
-			def flowdsl = new File("FlowPipeline.groovy").text
-			def body = new JsonBuilder( [overwrite: true, dsl: flowdsl] ).toString()			
-		}
+		])
+		
 	step([$class: 'ElectricFlowPipelinePublisher', 
 		configuration: 'Colocated Flow',
 		projectName: 'Default',
